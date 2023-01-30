071291793e674471f92a6993035a1b2d9fcee4d9
123 <123123123>
2023-01-30 10:23:40 +0300

    Добавьте файлы проекта.

---
+++ Classes/EFClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClothingStore_ISP9_13.BD;
+
+namespace ClothingStore_ISP9_13.Classes
+{
+    public class EFClass
+    {
+        public static Entities Context = new Entities();
+    }
+}
---
+++ Windows/EnterWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ClothingStore_ISP9_13.Classes;
+using ClothingStore_ISP9_13.BD;
+
+namespace ClothingStore_ISP9_13.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для EnterWindow.xaml
+    /// </summary>
+    public partial class EnterWindow : Window
+    {
+        public EnterWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            var AuthUser = EFClass.Context.User.ToList();
+
+
+        }
+    }
+}
